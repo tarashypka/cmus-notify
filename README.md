3e3c48f103ebf781d19062008b415a5f66beffe1
Taras Shypka <tarashypka@gmail.com>
2016-05-17 01:10:27 +0300

    Added script that sends notifications with 'last.fm love', 'add to favorites.pls' buttons

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
 # cmus-notify
-GTK script for CMux console media player
+#### *GTK script for CMux console media player*
+
+### It does:  
+  * send desktop notification about currently playing track;
+  * save currently playing track into the favorites playlist;
+  * send currently playing track love to last.fm
+  
+### Dependencies
+```
+python 2.7
+python-gtk2
+python-notify
+```
+Users of xfce4 can also use xfce4-notifyd-config to modify notification bar.  
+  
+### Installation
+```
+chmod u+x /path/to/notify-cmus.sh
+```
+In CMus command line:
+```
+set status_display_program=/path/to/notify-cmus.sh
+```
+or call the script from another one, f.e, it could be called from the `last-cmus` scrobbler with:  
+```python
+from subprocess import call
+call(["python", "/path/to/cmus-notify.py"])
+```
+  
+#### User-specific properties
+##### set yours system user name
+```
+user = "username"
+```
+##### set yours favorites.pls path
+```
+playlist = "/path/to/favorites.pls"
+```
+##### create last.fm API account and generate keys
+For now, to be able to send track love to last.fm, a specific API account should be generated for embedding the script with personal api and secret keys:  
+```
+APIKEY    = "apikey"
+SECRETKEY = "secretkey"
+```
+##### choose notification timeout (seconds)
+```
+timeout = 10
+```
+  
+### Possible issues
+Verify an output from `cmus-remote -C`. If there is an error, then try to replace the array  
+```python
+cmus = ["cmus-remote", "-C"] 
+```
+in `cmus-notify.py` with an array  
+```python
+cmus = ["cmus-remote", "--server", socket, "-C"]
+```
---
+++ cmus-notify.py
@@ -0,0 +1,143 @@
+#! /usr/bin/python
+
+# User-specific settings
+APIKEY    = "apikey"
+SECRETKEY = "secretkey"
+user = "username"
+playlist = "/home/" + user + "/Music/Playlists/favorites.pls"
+socket = "/home/" + user + "/.cmus/socket"
+timeout = 10
+
+URL = "http://ws.audioscrobbler.com/2.0/"
+AUTHURL = "http://www.last.fm/api/auth/"
+
+from subprocess import call, check_output
+from sys import exit
+from gtk import main, main_quit
+from gobject import timeout_add
+from os import path, listdir
+from re import match, compile
+
+import urllib2
+import json
+import hashlib
+import requests
+import pynotify
+import webbrowser
+import time
+
+cmus = ["cmus-remote", "-C"]
+
+def cmuscall(arg):
+    if call(cmus + ["status"]) != 0:
+        exit(1)
+    return check_output(cmus + [arg])
+
+def grep(lines, patt):
+    for line in lines.splitlines():
+        if patt in line:
+            return line
+
+def gettoken():
+    tokenrequrl = URL \
+                + "?method=auth.getToken" \
+                + "&api_key=" + APIKEY \
+                + "&format=json"
+    response = urllib2.urlopen(tokenrequrl)
+    jsondata = json.load(response)
+    token = jsondata["token"]
+
+    # Request authorization from the user
+    authurl = AUTHURL \
+            + "?api_key=" + APIKEY \
+            + "&token=" + token
+    webbrowser.open_new_tab(authurl)
+    time.sleep(30)
+
+    return token
+
+def getapisig(token):
+    sig = "api_key" + APIKEY \
+        + "method" + "auth.getSession" \
+        + "token" + token \
+        + SECRETKEY
+    h = hashlib.new('md5')
+    h.update(sig)
+    return h.hexdigest()
+
+def getseskey():
+    if path.isfile('seskey'):
+        print "Already have a session key"
+        seskeyf = open('seskey', 'r')
+        seskey = seskeyf.read()
+    else:
+        print "Generate new session key"
+        token = gettoken()
+        apisig = getapisig(token)
+        sesrequrl = URL \
+                  + "?method=auth.getSession" \
+                  + "&api_key=" + APIKEY \
+                  + "&api_sig=" + apisig \
+                  + "&token=" + token \
+                  + "&format=json"
+        response = urllib2.urlopen(sesrequrl)
+        jsondata = json.load(response)
+        seskey = jsondata["session"]["key"]
+        seskeyf = open('seskey', 'w')
+        seskeyf.write(seskey)
+    seskeyf.close()
+    return seskey
+
+def favorite(n, action):
+    assert action == "favorite"
+    if not n.show():
+        exit(1)
+    cmuscall("view 3")
+    cmuscall("load " + playlist)
+    cmuscall("view 1")
+    cmuscall("win-sel-cur")
+    cmuscall("win-add-p")
+    cmuscall("view 3")
+    cmuscall("save " + playlist)
+
+def sendlove(n, action):
+    assert action == "sendlove"
+    if not n.show():
+	exit(1)
+    seskey = getseskey()
+    cmusresp = cmuscall("status")
+    artist = grep(cmusresp, "tag artist ")[11:]
+    title = grep(cmusresp, "tag title ")[10:]
+    data = { "api_key": APIKEY, \
+             "artist": artist, \
+             "method": "track.love", \
+             "sk": seskey, \
+             "track": title }
+    res = requests.post(URL, data)
+
+if not pynotify.init("Cmus Notification"):
+    n.close()
+    exit(1)
+
+if __name__ == '__main__':
+    cmusresp = cmuscall("status")
+    title = grep(cmusresp, "tag title ")[10:]
+    album = grep(cmusresp, "tag album ")[10:]
+    artist = grep(cmusresp, "tag artist ")[11:]
+    file_path = grep(cmusresp, "file ")[5:]
+    file_path = file_path.rsplit('/', 1)[0]
+    icon_path = file_path + "/"
+    for file in listdir(file_path):
+        if compile(".*\.jpe?g").match(file):
+            icon_path += file
+    icon_uri = "file://" + icon_path
+    n = pynotify.Notification(title, artist + "\n" + album, icon_uri)
+    n.add_action("favorite", "Add to favorites", favorite)
+    n.add_action("sendlove", "Send Last.fm love", sendlove)
+    n.set_timeout(timeout * 1000)
+
+    if not n.show():
+        exit(1)
+
+    timeout_add(2 * timeout * 1000, main_quit)
+    main()
